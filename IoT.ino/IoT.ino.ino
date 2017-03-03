47e62aaa1feddc120631a914eb7e24302107fef5
Diovane Soligo <diovane_soligo@hotmail.com>
2017-03-03 17:43:32 -0300

    03/2017 Enviando dados para ServerSocket
    falta fazer a parte de receber comandos para ativar ou desativar a
    tomada

--- IoT.ino/IoT.ino.ino
+++ IoT.ino/IoT.ino.ino
@@ -9,10 +9,10 @@ LiquidCrystal lcd(9, 8, 7, 6, 5, 3);
 
 //************Config p/ Ethernet Shield****
 byte mac[] = { 0x90, 0xA2, 0xDA, 0x0E, 0x0C, 0xF1 }; //em exadecimal 0x + nº
-byte ip[] = { 192 , 168 , 0 , 110 };                      // IP do protótipo
-byte gateway[] = { 192 , 168, 0, 1 };                //IP do gateway
-byte subnet[] = { 255, 255, 255, 0 };                //Máskara de Subrede
-byte servidor[] = { 192 , 168 , 0 , 187 };            //Servidor JAVA
+byte ip[] = { 192 , 168 , 90 , 119 };                      // IP do protótipo
+byte gateway[] = { 192 , 168, 80, 1 };                //IP do gateway
+byte subnet[] = { 255, 255, 240, 0 };                //Máskara de Subrede
+byte servidor[] = { 192 , 168 , 90 , 210 };            //Servidor JAVA
 EthernetServer server(1000);                         //Acesso pela porta 1000
 EthernetClient cliente;
 
@@ -21,7 +21,7 @@ int CORRENTE_PIN = A5; //Sensor de Corrente no pino A4 (alterar pino projeto)
 Tensao tensao(A4);
 //**********Saidas*************************
 int PIN_RELE = A2;
-int id = 1; //id do projeto
+String id = "001"; //id do projeto
 //***********Calibracao********************
 EnergyMonitor corrente;
 //****************************************
@@ -72,10 +72,6 @@ void loop() {
   Serial.print(pino); //tensão.valor() - valor lido pelo sensor real-time
   Serial.println(" volts");
 
-  //Serial.print("Tensao Armazena: ");
-  //Serial.print(volts);
-  //Serial.println(" volts");
-
   Serial.print("Potencia: ");
   Serial.print(volts * Irms);
   Serial.println(" Watts\n****************\n");
@@ -102,26 +98,30 @@ void loop() {
   lcd.print(" | W: ");
   lcd.print(volts * Irms);
   lcd.print("    ");
+
+  String V = String(volts);
+  String C = String(Irms);  
+
+  String ENVIA = "GET /?ID=";
+  ENVIA.concat(id);
+  ENVIA.concat("&VOLTS=");
+  ENVIA.concat(V);
+  ENVIA.concat("&CORRENTE=");
+  ENVIA.concat(C);
+
+  //ENVIA = "GET /?ID=1@1&VOLTS=219&CORRENTE=0,1123";
+  
   if (cliente.connect(servidor, 8080)) {
     Serial.println("CONECTADO...");
-
-    cliente.print("GET /serverIoT/recebe_dados?");
-    cliente.print("VCA=");
-    cliente.print(volts);
-    cliente.print("&ACA=");
-    cliente.print(Irms);
-    cliente.print("&ID=");
-    cliente.print(id);
+    
+    cliente.print(ENVIA);
     cliente.println(" HTTP/1.1");
 
     cliente.println("Host: www.google.com");
     cliente.println("Connection: close");
     cliente.println();
-
-    //cliente.stop();
   } else {
     Serial.println("ERRO NA CONEXAO...");
-    //cliente.stop();
   }
 
   delay(1000);
